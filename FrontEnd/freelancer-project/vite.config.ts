c0058da6b559afb1652cce954db4f9704f59a630
黄栋君 <huangdongjun@Mac-mini.local>
2025-10-22 21:29:11 +0800

    Fix issue

--- FrontEnd/freelancer-project/vite.config.ts
+++ FrontEnd/freelancer-project/vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
-  base: '/app/', // ← 开发和部署都以 /app/ 为基座
+  base: '/', 
   plugins: [vue()],
 }))
